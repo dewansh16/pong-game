b7b754ecf84779acf485bf31eba63f82d0a700a7
Dewansh Patle <dewanshpatle69@gmail.com>
2023-02-05 00:50:37 +0530

    ready to deploy

---
+++ api.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const api = express();
+
+api.use(express.static(path.join(__dirname, "public")));
+
+api.use("/", express.static("index.html"));
+
+api.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+module.exports = api;
